671ba6c6cbe450108b5a1b092c441e176273b400
Nguyen Trong Quy <trongquy131204@gmail.com>
2025-01-14 15:14:06 +0700

    Video Card - commit 1
    Video cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import './App.css';
+import VideoCard from "./components/VideoCard";
 
-import './App.css'
+// Dynamically import all images in the 'pictures' folder
+const images = import.meta.glob('./assets/images/thumbnails/*.jpg', { eager: true }) as Record<
+string,
+{ default: string }
+>;
+
+// Extract the image paths
+const imagePaths = Object.values(images).map((img) => img.default);
 
 function App() {
 
   return (
     <>
-    
+    <div className='videos-container'>
+      {imagePaths.map((src, index) => (
+        <VideoCard key={index} thumbnail={src}/>
+      ))}
+    </div>
+
     </>
   )
 }

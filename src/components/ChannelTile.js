de39079d3028fe776033b4c3ff329e191b4ab74e
DanieLsvenn <seizurebabe@gmail.com>
2025-01-14 14:07:37 +0700

    ChannelTile and VideoTile components

---
+++ src/components/ChannelTile.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "ChannelTile.css"
+import { NotificationsActive } from '@mui/icons-material'
+
+function ChannelTile() {
+    return (
+        <div className='channelTile'>
+            <div className='tile_left'>
+                <img className='channelImg' src='https://yt3.ggpht.com/ytc/AKedOLRk8g9KQ9K5X9pD4Y5XbBZo2Ft3U3Z7uZjJZ6k=s88-c-k-c0x00ffffff-no-rj' alt='' />
+            </div>
+
+            <div className='tile_middle'>
+                <p className='channelName'>FLEEKAZOID</p>
+                <p style={{ fontSize: "13px", color: "#606060", letterSpacing: "0.3px" }}>
+                    1.5m subscribers &bull; 162 videos
+                </p>
+            </div>
+
+            <div className='tile_right'>
+                <button className='subscribe'>SUBSCRIBE</button>
+
+                <NotificationsActive className='bellIcon' />
+            </div>
+        </div>
+    )
+}
+
+export default ChannelTile
